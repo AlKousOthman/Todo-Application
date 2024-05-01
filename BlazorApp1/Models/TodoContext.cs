f202f39d13530e27376eb021e59d0657797c17b7
new othman <new othman@OTHMAN>
2024-05-01 13:33:58 +0300

    Add project files.

---
+++ BlazorApp1/Models/TodoContext.cs
@@ -0,0 +1,30 @@
+﻿using BlazorApp1.Models.Entitiy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+namespace BlazorApp1.Models
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<TodoItem> Items { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().HasData(new List<TodoItem>{
+             new TodoItem{ Description = "Complete all Coded Assignments", IsCompleted = false, Id = 1, DueDate = new DateTime(2024,04,30)},
+             new TodoItem{ Description = "Appartment Rent", IsCompleted = true, Id = 2},
+             new TodoItem{ Description = "Clean My Room", IsCompleted = false, Id = 3}
+ });
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                base.OnConfiguring(optionsBuilder);
+                optionsBuilder.UseSqlite("Data Source=Todo.db");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
